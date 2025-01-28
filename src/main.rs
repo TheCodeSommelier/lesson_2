aae6923a559663ac403be5fc48b9890c8a9d712a
Tony Masek <tm@tony-masek.com>
2025-01-28 19:44:32 +0100

    Made empty arguments possible

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,16 @@ use slug::slugify;
 use std::env;
 use std::io;
 
-fn main() {
-    let mut user_input = String::new();
-
-    println!("Hey give me a string to slugify!");
-
-    io::stdin()
-        .read_line(&mut user_input)
-        .expect("Failed to read line...");
-
-    let used_arg: Vec<String> = env::args().collect();
-    let used_arg = &used_arg[1];
-
-    if used_arg.is_empty() {
-        println!("{}", user_input);
-    } else {
-        println!("{}", use_env_arg(&used_arg, user_input));
-    }
-}
-
-fn use_env_arg(used_arg: &String, user_input: String) -> String {
+pub fn use_env_arg(used_arg: &String, user_input: String) -> String {
     match used_arg as &str {
-        "lowercase" => user_input.to_lowercase(),
-        "uppercase" => user_input.to_uppercase(),
-        "no-spaces" => user_input.replace(" ", ""),
-        "slugify" => slugify(user_input),
-        "go-nuts" => go_nuts(user_input),
-        _ => "This is all kinds of weird".to_string(),
-    }
+    "lowercase" => user_input.to_lowercase(),
+    "uppercase" => user_input.to_uppercase(),
+    "no-spaces" => user_input.replace(" ", ""),
+    "slugify" => slugify(user_input),
+    "go-nuts" => go_nuts(user_input),
+    "how-long" => "This took me 19 days to make".to_string(),
+    _ => "The argument provided is all kinds of weird... Try again... Pick either one of these \n1. lowercase \n2. uppercase \n3. no-spaces \n4. slugify \n5. go-nuts".to_string(),
+}
 }
 
 fn go_nuts(user_input: String) -> String {
@@ -42,4 +24,24 @@ fn go_nuts(user_input: String) -> String {
     let picked_arg = potenial_args[random_num].to_string();
     println!("{} was picked!", picked_arg);
     return use_env_arg(&picked_arg, user_input);
+}
+
+fn main() {
+    let mut user_input = String::new();
+
+    println!("Hey give me a string to format!");
+
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Failed to read line...");
+
+    let mut used_args: Vec<String> = env::args().collect();
+    used_args.remove(0);
+
+    if used_args.is_empty() {
+        println!("No arguments were given: {}", user_input);
+    } else {
+        let used_arg = &used_args[0];
+        println!("Here is the formated string: {}", use_env_arg(&used_arg, user_input));
+    }
 }
